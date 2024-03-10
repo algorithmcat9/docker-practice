ccfd82a5521d17bf38f30d9aede561e8a66e00f0
algorithmcat <algorithmcat9@gmail.com>
2024-03-10 05:02:20 +0500

    Подключение к mysql, создание бд, таблиц через терминал, сохранение данных бд

--- src/index.php
+++ src/index.php
@@ -1,2 +1,12 @@
 <?php
-phpinfo();
+// phpinfo();
+
+$conn = mysqli_connect('mysql', 'root', 'root');
+
+// Создание БД и проверка подключения 
+$sql = "CREATE DATABASE testdb2";
+if($conn->query($sql)){
+    echo "База данных успешно создана";
+} else{
+    echo "Ошибка: " . $conn->error;
+}
